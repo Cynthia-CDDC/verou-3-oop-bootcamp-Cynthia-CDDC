83b8b83bd85d51ef2e9b4b38991d8b1e4a18f6c2
CDDC <cynthia.decelle@gmail.com>
2022-02-24 16:19:06 +0100

    Complete ex-1-A

--- case-1-A/idex.php
+++ case-1-A/idex.php
@@ -1,54 +1,29 @@
 <?php
-function whatIsHappening() {
-    echo '<h2>$_GET</h2>';
-    var_dump($_GET);
-    echo '<h2>$_POST</h2>';
-    var_dump($_POST);
-    echo '<h2>$_COOKIE</h2>';
-    var_dump($_COOKIE);
-    echo '<h2>$_SESSION</h2>';
-    var_dump($_SESSION);
-}
-whatIsHappening();
-
-// create array with product-name, price-pretax, taxrate, nr-of-pieces.
-// create function for tax calculation
-//TODO: addition the pretax prices
-//TODO: addition the incl tax prices = total
-//TODO: incl tax prices - pretax prices = total tax
-
 $products = [
-    ['name' => 'Banana', 'pricePretax' => 0.94, 'taxrate'=> 6, 'numPieces'=>6],
-    ['name' => 'Apple', 'pricePretax' => 1.41, 'taxrate'=> 6, 'numPieces'=>3],
-    ['name' => 'WineBottle', 'pricePretax' => 8.26,'taxrate'=> 21, 'numPieces'=>2],
+    ['name' => 'Banana', 'pricePretax' => 0.94, 'taxrate'=> 1.06, 'numPieces'=>6],
+    ['name' => 'Apple', 'pricePretax' => 1.41, 'taxrate'=> 1.06, 'numPieces'=>3],
+    ['name' => 'WineBottle', 'pricePretax' => 8.26,'taxrate'=> 1.21, 'numPieces'=>2],
 ];
 
-$pretaxTotal = 0;
-
-function createPriceInclTax($products, $pretaxTotal)
+function createPriceInclTax($products)
 {   
+    $currentTotal = 0;
+    $taxTotal = 0;
     foreach ($products as $product){
+        $productTotalIncl = $product['taxrate'] * ($product['pricePretax'] * $product['numPieces']);
+        $productTotalExl = $product['pricePretax'] * $product['numPieces'];
         
-        if($product['taxrate'] == 6){
-            $priceInclTax = $product['pricePretax'] * 1.06;
-        }
-        if($product['taxrate'] == 21){
-            $priceInclTax = $product['pricePretax'] * 1.21;
-        }
-        $preTaxtotalPerProduct = $product['pricePretax'] * $product['numPieces'];
-
-        echo '<h2>$_POST</h2>';
-        var_dump($preTaxtotalPerProduct);
-        echo '<h2>$_COOKIE</h2>';
-
-        echo '<h2>$_POST</h2>';
-        var_dump($priceInclTax);
-        echo '<h2>$_COOKIE</h2>';
+        $productTaxCost = $productTotalIncl - $productTotalExl;
+        
+        $currentTotal += $productTotalIncl;
+        $taxTotal += $productTaxCost;
     }
-    // echo '<h2>$_POST</h2>';
-    // var_dump($pretaxTotal += $preTaxtotalPerProduct);
-    // echo '<h2>$_COOKIE</h2>';
-    // echo ($products[1]['name']);
+    echo '<h2>$total</h2>';
+    var_dump($currentTotal);
+    echo '</br>';
+    echo '===============';
+    echo '</br>';
+    var_dump($taxTotal);
 }
 
 createPriceInclTax($products);

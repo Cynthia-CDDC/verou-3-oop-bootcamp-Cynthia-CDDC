2db39bab36a7912e24098afab190fb6d0ecb4024
CDDC <cynthia.decelle@gmail.com>
2022-02-24 10:27:22 +0100

    Create in php todo's for case-1-A

---
+++ case-1-A/idex.php
@@ -0,0 +1,6 @@
+<?php
+//TODO: create array with product-name, price-pretax, taxrate, nr-of-pieces.
+//TODO: create function for tax calculation
+//TODO: addition the pretax prices
+//TODO: addition the incl tax prices = total
+//TODO: incl tax prices - pretax prices = total tax
